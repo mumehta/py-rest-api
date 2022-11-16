d1ec6fbf3ccfda45e44f3aa57d501bfe882e7d7c
Munish Mehta <munish.mehta.au@gmail.com>
2022-11-16 17:32:53 -0500

    remove redundents

--- people.py
+++ people.py
@@ -4,29 +4,7 @@ from flask import abort, make_response
 import urllib.request, pymysql
 import json
 
-
-def get_timestamp():
-    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
-
 url = "https://my-json-server.typicode.com/mumehta/py-rest-api/db"
-
-PEOPLE = {
-    "Fairy": {
-        "fname": "Tooth",
-        "lname": "Fairy",
-        "timestamp": get_timestamp(),
-    },
-    "Ruprecht": {
-        "fname": "Knecht",
-        "lname": "Ruprecht",
-        "timestamp": get_timestamp(),
-    },
-    "Bunny": {
-        "fname": "Easter",
-        "lname": "Bunny",
-        "timestamp": get_timestamp(),
-    },
-}
 
 def show_people():
     with urllib.request.urlopen(url) as u:
